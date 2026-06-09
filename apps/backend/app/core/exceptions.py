f80324a1a4c3110677ca12a3124cf6f93ba0033f
Shervin Schneiser <shervinschneiser@gmail.com>
2026-06-10 00:43:46 +0330

    refactor: move worker package into backend service

--- apps/backend/app/core/exceptions.py
+++ apps/backend/app/core/exceptions.py
@@ -1,15 +1,31 @@
+import traceback
 from fastapi import Request
 from fastapi.responses import JSONResponse
+
+
+# async def global_exception_handler(
+#     request: Request,
+#     exc: Exception,
+# ):
+#     return JSONResponse(
+#         status_code=500,
+#         content={
+#             "success": False,
+#             "detail": "Internal server error",
+#         },
+#     )
 
 
 async def global_exception_handler(
     request: Request,
     exc: Exception,
 ):
+    traceback.print_exc()
+
     return JSONResponse(
         status_code=500,
         content={
             "success": False,
-            "detail": "Internal server error",
+            "detail": str(exc),
         },
     )
