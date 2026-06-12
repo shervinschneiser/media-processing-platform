891ef586246f612f5af9b82961fc5c4e7d104dcd
Shervin Schneiser <shervinschneiser@gmail.com>
2026-06-12 23:04:23 +0330

    feat(worker): add job processing task

--- apps/backend/workers/tasks.py
+++ apps/backend/workers/tasks.py
@@ -1,5 +1,5 @@
 from app.core.celery_app import celery_app
 
-@celery_app.task(name="test_task")
-def test_task():
-    return "Hello, World from celery"
+@celery_app.task(name="process_job")
+def process_job(job_id: int):
+    print(f"processing job {job_id}")
